524cfc17ea60a14752f4775d557191780679e08c
Elena Andreeva <lenochka1551@yandex.ru>
2024-07-29 20:04:05 +0300

    проверка проекта

---
+++ src/components/card.js
@@ -0,0 +1,38 @@
+
+ const cardTemplate = document.querySelector('#card-template').content
+
+// //@todo: Функция создания карточки
+function createCard(element, deleteCard, likeOnCard, openPopupImage) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true)
+
+    const cardDelete = cardElement.querySelector('.card__delete-button')
+    const cardLike = cardElement.querySelector('.card__like-button')
+    const cardImage = cardElement.querySelector('.card__image')
+    const cardTitle = cardElement.querySelector('.card__title')
+
+    cardImage.src = element.link;
+    cardTitle.textContent = element.name;
+    cardImage.alt = `Изображение ${element.name}`;
+
+    cardImage.addEventListener('click', () => {
+        openPopupImage(element)
+    })
+    cardDelete.addEventListener('click', deleteCard)
+    cardLike.addEventListener('click', likeOnCard) 
+     
+   
+return cardElement
+}
+
+// @todo: Функция удаления карточки
+function deleteCard(event) {
+    let card = event.target.closest('.card')
+    card.remove()
+}
+
+//функция лайка на карточке
+function likeOnCard(evt) {
+    evt.target.classList.toggle('card__like-button_is-active')
+  }
+
+  export {createCard, deleteCard, likeOnCard}
--- src/index.js
+++ src/index.js
@@ -1,44 +1,94 @@
 
+// import { event } from 'jquery';
 import '../pages/index.css';
-// import { initialCards } from './cards.js'
-
-// @todo: Темплейт карточки
-const cadrTemplate = document.querySelector('#card-template').content; 
-const placeItem = document.querySelector('.places__item');
-
+import { initialCards } from './cards.js';
+import { openPopup, closePopup } from './components/modal.js';
+import { createCard, deleteCard, likeOnCard } from './components/card.js';
 
 
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
+const profileEditBtn = document.querySelector('.profile__edit-button');
+const profileAddBtn = document.querySelector('.profile__add-button')
+const popupTpEdit = document.querySelector('.popup_type_edit')
+const popupTpNewCard = document.querySelector('.popup_type_new-card')
+const popupImage = document.querySelector('.popup__image')
+const popupTpImage = document.querySelector('.popup_type_image')
+const popupCaption = document.querySelector('.popup__caption')
+const profileTitle = document.querySelector('.profile__title')
+const profileDescription = document.querySelector('.profile__description')
+const formdEditProfile = document.forms['edit-profile']
+const nameInput = document.querySelector('.popup__input_type_name')
+const jobInput = document.querySelector('.popup__input_type_description')
+const popupCloseBtn = document.querySelectorAll('.popup__close')
+const popup = document.querySelector('.popup')
+const linkInput = document.querySelector('.popup__input_type_url')
+const formNewPlace = document.forms['new-place']
+const cardNameInput = document.querySelector('.popup__input_type_card-name')
 
 
-//@todo: Функция создания карточки
-function createCard(element, deleteCard) {
-    const cardElement = cadrTemplate.querySelector('.places__item').cloneNode(true);
-    const cardDelete = cardElement.querySelector('.card__delete-button');
 
-    const cardImage = cardElement.querySelector('.card__image')
-    const cardTitle = cardElement.querySelector('.card__title')
+//вывожу карточки на страницу
+initialCards.forEach(function (element) {
+  const newCard = createCard(element, deleteCard, likeOnCard, openPopupImage) 
+  placesList.append(newCard)
+})
 
-    cardImage.src = element.link;
-    cardTitle.textContent = element.name;
-    cardImage.alt = `Изображение ${element.name}`;
 
-    cardDelete.addEventListener('click', deleteCard) 
-     
-   
-return cardElement
-}
+  //обработчик открытия модального окна по кнопке (редактирование профиля)
+  profileEditBtn.addEventListener('click', () => {
+    openPopup(popupTpEdit)
 
-// @todo: Функция удаления карточки
-function deleteCard(event) {
-    let card = event.target.closest('.card')
-    card.remove()
-    
-}
+    nameInput.value = profileTitle.textContent
+    jobInput.value = profileDescription.textContent
+  })
 
-// @todo: Вывести карточки на страницу
-initialCards.forEach(function(element) {
-const newCard = createCard(element, deleteCard)
-placesList.append(newCard)
-});
+  //обработчик открытия модального окна по кнопке добавить
+  profileAddBtn.addEventListener('click', () => {
+    openPopup(popupTpNewCard)
+  } )
+
+
+  //функция октрытия попапа по клику на картинку
+  function openPopupImage (item) {
+    popupImage.src = item.link
+    popupImage.alt = item.name
+    popupCaption.textContent = item.name
+    openPopup(popupTpImage)
+  }
+
+  //функция отмены стандартной отправки формы
+  function handleFormSubmitProfile(evt) {
+    evt.preventDefault();
+
+    profileTitle.textContent = nameInput.value
+    profileDescription.textContent = jobInput.value
+
+    closePopup(popupTpEdit)
+  }
+  formdEditProfile.addEventListener('submit', handleFormSubmitProfile); 
+
+  
+  //обработчик закрытия попапов по нажатию на крестик
+  popupCloseBtn.forEach(event => {
+     const popup = event.closest('.popup')
+    event.addEventListener('click', () => {
+      closePopup(popup)
+    })
+  });
+ 
+   //добавление новой карточки в начало и очистка формы
+   function createNewCard (event) {
+    event.preventDefault();
+
+    const element = {
+      name: cardNameInput.value,
+      link: linkInput.value,
+    }
+    const newCardPopup = createCard(element, deleteCard, likeOnCard, openPopupImage)
+
+    placesList.prepend(newCardPopup)
+    closePopup(popupTpNewCard)
+    event.target.reset()
+   }
+formNewPlace.addEventListener('submit', createNewCard)
